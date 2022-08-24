5b96c4468949d55594257a6701f8c8a6ea9f55df
hambalee <hambalee@outlook.com>
2022-08-25 06:03:31 +0700

    update server url

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -245,7 +245,7 @@ const AppProvider = ({ children }) => {
 
   //?! Fetch Data Activity ------------------------------------------
 
-  const url = import.meta.env.VITE_SERVER_URL
+  const url = "https://final-project-generation-backend.vercel.app/"
 
   const body = { token: localStorage.getItem("token") };
   console.log("body in context : ", body);
